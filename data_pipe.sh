c0ea67950572d12f6a04adc5a85dcd6723076848
Peter Edsberg Mollgaard <petem@heisenberg.imm.dtu.dk>
2020-06-02 22:15:12 +0200

    Updating gitignore and pipe

--- data_pipe.sh
+++ data_pipe.sh
@@ -2,9 +2,9 @@
 cd ~/HOPE/WorldCovid19/
 
 # execute all python scripts
-/home/petem/Applications/anaconda3/bin/python3 pull_data/pull.py
+# /home/petem/Applications/anaconda3/bin/python3 pull_data/pull.py
 /home/petem/Applications/anaconda3/bin/python3 preprocess_everything.py;
 
 # then build the website
-cd covid19.compute.dtu.dk/static;
-rsync -av data petem@thinlinc.compute.dtu.dk:/www/sites/covid19.compute.dtu.dk;
+# cd covid19.compute.dtu.dk/static;
+# rsync -av data petem@thinlinc.compute.dtu.dk:/www/sites/covid19.compute.dtu.dk;
