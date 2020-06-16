e7aba8b9757f0acc4d86ab28c720bbb373f9a6fa
Udneowi <peter-em@hotmail.com>
2020-06-16 09:29:03 +0200

    Making sure locations from the shapefiles have the right form

--- utils/geo_utils.py
+++ utils/geo_utils.py
@@ -51,7 +51,8 @@ def create_shape_file(country, adm, save_dir=False, file_return=True, return_geo
                 geodf = gpd.GeoDataFrame.from_features(collection, crs="epsg:4326")
                 columns_replace = geodf.columns[geodf.columns.str.startswith('NAME_')]
                 for column in columns_replace:
-                    geodf[column] = geodf[column].str.replace('\W+',' ')
+                    geodf[column] = geodf[column].str.replace('\W+',' ').str.strip()
+                geodf = geodf.dropna(subset=columns_replace)
 
                 if return_geo_pd:
                     return geodf
