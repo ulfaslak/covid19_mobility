a4db95281d2554f46821ce0769287889779268bd
Udneowi <peter-em@hotmail.com>
2020-06-22 14:12:11 +0200

    Small bug fixes

--- utils/telco_utils/absolute_deviation.py
+++ utils/telco_utils/absolute_deviation.py
@@ -111,16 +111,11 @@ def run(country):
                 data_day_type = data_day[data_day['origin_area_code'] == data_day['destination_area_code']]
             
             # Add data to data_out
+            update_data_out(fn_time, 'all', data_day_type)
             for adm2 in set(data_day_type['source_kommune']):
                 update_data_out(fn_time, adm2, data_day_type[data_day_type['source_kommune'] == adm2])
 
-    for fn_time in ['between', 'within']:
-        # Filter
-        if fn_time == 'between':
-            data_type = data[data['origin_area_code'] != data['destination_area_code']]
-        elif fn_time == 'within':
-            data_type = data[data['origin_area_code'] == data['destination_area_code']]
-        update_data_out(fn_time, 'all', data_type)
+
 
     # Time
     data_out['_meta']['datetime'] = fn_days
