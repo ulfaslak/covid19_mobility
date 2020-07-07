031edbdb24165b3a5b9c7d7b164bccd4d2ee3fe4
Peter Edsberg Mollgaard <peter-em@hotmail.com>
2020-07-07 10:41:05 +0200

    Adding preprocessing script for telco data

---
+++ preprocess_telco.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+from joblib import Parallel, delayed
+import os
+import json
+
+from utils.telco_utils import movements_telco , absolute_deviation
+from utils.geo_utils import create_shape_file
+
+def run():
+    # Then run in parallel
+    print('\n-----------\nrun in parallel:\n----------------')
+    country = 'Denmark'
+    pscripts = [absolute_deviation, movements_telco]
+    Parallel(n_jobs=min(8, len(pscripts)))(delayed(lambda x: x.run(country))(x) for x in pscripts)
+            
+
+if __name__ == "__main__":
+    run()
